27f11b381f41a1ddff82b3391a781da52d95895e
Benoit Nathaël <152443375+nathaelbenoit@users.noreply.github.com>
2025-04-10 16:04:39 +0200

    importation de tous les fichiers

---
+++ selmarin.py
@@ -0,0 +1,141 @@
+import mysql.connector
+import csv
+from datetime import datetime
+from tkinter import *
+from tkinter import messagebox
+from tkinter import ttk
+import mysql.connector
+import os
+
+def long(longueur : int) -> str:
+    '''Retourne le texte correspondant à la longueur de la liste.'''
+    if longueur == 1:
+        text = "(%s)"
+    elif longueur == 2:
+        text = "(%s, %s)"
+    elif longueur == 3:
+        text = "(%s, %s, %s)"
+    elif longueur == 4:
+        text = "(%s, %s, %s, %s)"
+    elif longueur == 5:
+        text = "(%s, %s, %s, %s, %s)"
+    return text
+
+def ajout(nomBd, row):
+    '''Ajoute une ligne à la base de données.'''
+    db = mysql.connector.connect(host="localhost", user="root", password="", database="selmarin_final")
+    db.autocommit = True
+    with db.cursor() as c:
+        try:
+            c.execute("INSERT INTO " + nomBd + " VALUES " + long(len(row)), tuple(row))
+            print(nomBd + " : Ligne ajoutée")
+        except mysql.connector.errors.IntegrityError:
+            print(nomBd + " : La clé primaire déjà existante OU clé étrangère inexistante")
+    db.close()
+
+def ajoutExcelBd(nomFichier, longueur):
+    '''Ajoute les données d'un fichier CSV à la base de données.'''
+    db = mysql.connector.connect(host="localhost", user="root", password="", database="selmarin_final")
+    db.autocommit = True
+    
+    long(longueur)
+
+    try:
+        with open(nomFichier +".csv", 'r') as file:
+            reader = csv.reader(file, delimiter=";")
+            # Sauter l'en-tête si nécessaire (si le fichier CSV a des titres de colonnes)
+            header = next(reader)
+            for row in reader:
+                if nomFichier == "entree" or nomFichier == "sortie":
+                    date = list(row[1])
+                    date[6:10], date[3:5], date[:2] = date[:2], date[3:5], date[6:10]
+                    date = ''.join(date)
+                    row[1] = datetime.strptime(date, '%Y/%m/%d')
+            
+                if nomFichier == "sortie":
+                    rowConcerner = [row[3]] + [row[0]] + [row[4]]
+                    row = row[:3]
+                    ajout("concerner", rowConcerner)
+                
+                ajout(nomFichier, row)
+        messagebox.showinfo("Bonne exécution", "Les tables ont été correctement remplies")
+
+    except FileNotFoundError:
+        print(f"Erreur : Le fichier {nomFichier} n'a pas été trouvé.")
+    except Exception as e:
+        print(f"Erreur lors de la lecture du fichier {nomFichier} : {e}")
+
+    # Fermeture de la connexion à la base de données
+    db.close()
+
+def lister_fichiers_csv():
+    '''Liste les fichiers CSV dans le répertoire courant.'''
+    fichiers_csv = [f[:-4] for f in os.listdir('.') if f.endswith('.csv')]
+    return fichiers_csv
+
+def nombre_colonnes(fichier_csv):
+    '''Retourne le nombre de colonnes du fichier CSV.'''
+    try:
+        with open(fichier_csv + ".csv", newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                row_split = row[0].split(';')
+                return len(row_split)
+    except FileNotFoundError:
+        print(f"Erreur : Le fichier {fichier_csv}.csv n'existe pas.")
+    except Exception as e:
+        print(f"Erreur lors de la lecture du fichier {fichier_csv}.csv : {e}")
+
+def executer_requete(variable_requete, texte_requete, afficher_resultats, trouverSQL, fenetre):
+    '''Exécute une requête SQL.'''
+    requete = trouverSQL(variable_requete.get())
+    requete_personnalisee = texte_requete.get("1.0", "end").strip()
+    if requete_personnalisee:
+        requete = requete_personnalisee
+    if requete == "NONE":
+        return messagebox.showwarning("Attention", "Veuillez sélectionner une requête SQL.")
+    try:
+        db = mysql.connector.connect(host="localhost", user="root", password="", database="selmarin_final")
+        db.autocommit = True
+        if requete[:6].upper() != 'UPDATE' and requete[:6].upper() != 'INSERT'  and requete[:6].upper() != 'DELETE':
+            with db.cursor() as c:
+                c.execute(requete)
+                colonnes = [desc[0] for desc in c.description]
+                resultats = c.fetchall()
+            db.close()
+            afficher_resultats(colonnes,resultats,fenetre)
+        else:
+            with db.cursor() as c:
+                c.execute(requete)
+            db.close()
+            messagebox.showinfo("Bonne exécution", "Requête exécutée avec succès")
+
+    except Exception as e:
+        messagebox.showerror("Erreur", f"Une erreur s'est produite : {e}")
+
+def afficher_resultats(colonnes, resultats, fenetre):
+    fenetre_resultats = Toplevel(fenetre)
+    fenetre_resultats.title("Résultats de la requête SQL")
+
+    tree = ttk.Treeview(fenetre_resultats, columns=colonnes, show="headings")
+
+    # Ajouter les en-têtes
+    for col in colonnes:
+        tree.heading(col, text=col, anchor="center")
+        tree.column(col, anchor="center", width=150)  # Ajuste les largeurs si nécessaire
+
+    # Insérer les données dans le Treeview
+    for ligne in resultats:
+        tree.insert("", "end", values=ligne)
+
+    scrollbar = ttk.Scrollbar(fenetre_resultats, orient="vertical", command=tree.yview)
+    tree.configure(yscroll=scrollbar.set)
+
+    tree.pack(padx=10, pady=10, fill="both", expand=True)
+    scrollbar.pack(side="right", fill="y")
+    
+def trouverSQL(text, liste_sql, requetes_sql):
+    '''Trouve la requête SQL correspondant au texte.'''
+    index = liste_sql.index(text)
+    requete = requetes_sql[index]
+    return requete
---
+++ selmarin_ihm.py
@@ -0,0 +1,102 @@
+from tkinter import *
+from tkinter import messagebox
+from selmarin import *
+from tkinter import ttk
+
+
+if __name__ == "__main__":
+    fenetre = Tk()
+    fenetre.title("Interface selmarin")
+    
+    cadre_principal = Frame(fenetre)
+    cadre_principal.pack(padx=10, pady=10, fill="both")
+
+    # Cadre gauche pour les fonctionnalités CSV & requêtes
+    cadre_gauche = ttk.Frame(cadre_principal)
+    cadre_gauche.pack(side="left", padx=10, anchor="n")
+
+    # Cadre droit pour les boutons d'affichage de tables
+    cadre_droit = ttk.Frame(cadre_principal)
+    cadre_droit.pack(side="right", padx=10, anchor="n")
+    
+    # Section pour ajouter un fichier CSV
+    TextCsv = Label(cadre_gauche, text="Ajouter un fichier CSV à la base de données")
+    TextCsv.pack(pady=5)
+    
+    fichiersCsv = lister_fichiers_csv()
+    variableCSV = StringVar(fenetre)
+    variableCSV.set(fichiersCsv[0] if fichiersCsv else "")
+    
+    listeCsv = ttk.Combobox(cadre_gauche, textvariable=variableCSV, values=fichiersCsv, state="readonly")
+    listeCsv.pack(pady=5)
+    
+    boutonCsv = ttk.Button(cadre_gauche, text="Valider", command=lambda: ajoutExcelBd(variableCSV.get(), nombre_colonnes(variableCSV.get())))
+    boutonCsv.pack(pady=5)
+    
+    
+    
+    # Section pour exécuter des requêtes SQL
+    textRequete = Label(cadre_gauche, text="Exécuter une requête SQL")
+    textRequete.pack(pady=5)
+    
+    nomsSQL = ["",
+                "Prix moyen des produits par tonne",
+                "Clients n'ayant pas commandé certains produits",
+                "Classement des clients qui ont le plus commandé",
+                "Stock restant par produit",
+                "Saunier ayant fourni le plus de sel",
+                "Chiffre d'affaires total par année",
+                "Clients ayant passé plus de 3 commandes",
+                "Renommer le client 11 en INTERMARCHÉ",
+                "Classement des produits par nb de commandes",
+                "Évolution des prix entre 2023 et 2025"]
+    requeteSQL = [
+        "NONE",
+        "SELECT libPdt, AVG(prixVente) As prix_moyen_T FROM prix, produit  WHERE produit.NUMPDT = prix.NUMPDT GROUP BY prix.NUMPDT;",
+        "SELECT client.nomCli, client.precisionCli, client.villeCli, (SELECT libPdt FROM produit WHERE NUMPDT = 1) AS produit_non_commande FROM client WHERE NUMCLI NOT IN (SELECT DISTINCT s.NUMCLI FROM sortie s, concerner c WHERE s.NUMSORT = c.NUMSORT AND c.NUMPDT = 1);",
+        "SELECT client.nomCli, client.precisionCli, client.villeCli, SUM(concerner.qteSort_t_ * prix.prixVente) AS somme_achats_euros FROM client, sortie, concerner, prix WHERE client.NUMCLI = sortie.NUMCLI AND sortie.NUMSORT = concerner.NUMSORT AND concerner.NUMPDT = prix.NUMPDT AND prix.numAnnee = YEAR(sortie.dateSort) GROUP BY client.NUMCLI ORDER BY somme_achats_euros DESC;",
+        "SELECT p.libPdt,(SUM(e.qteEnt__t_) - SUM(c.qteSort_t_)) AS stock_restant FROM produit p, entree e, concerner c WHERE p.NUMPDT = e.NUMPDT AND p.NUMPDT = c.NUMPDT GROUP BY p.NUMPDT;",
+        "SELECT s.nomSau, s.prenomSau, s.villeSau, SUM(c.qteSort_t_) AS total_sel_fournit FROM saunier s, concerner c, sortie so, entree e WHERE e.NUMSAU = s.NUMSAU AND c.NUMSORT = so.NUMSORT AND c.NUMPDT = 1  AND e.NUMPDT = c.NUMPDT GROUP BY s.NUMSAU ORDER BY total_sel_fournit DESC;",
+        "SELECT a.numAnnee, SUM((p.prixVente * c.qteSort_t_)) AS chiffre_affaires_total FROM annee a, prix p, concerner c, sortie so WHERE p.numAnnee = a.numAnnee AND c.NUMSORT = so.NUMSORT AND c.NUMPDT = p.NUMPDT GROUP BY a.numAnnee ORDER BY a.numAnnee DESC;",
+        "SELECT cl.nomCli, COUNT(so.NUMSORT) AS nombre_achats FROM client cl, sortie so WHERE cl.NUMCLI = so.NUMCLI GROUP BY cl.NUMCLI HAVING COUNT(so.NUMSORT) > 3 ORDER BY nombre_achats DESC;",
+        "UPDATE client SET nomCli = 'INTERMARCHÉ' WHERE NUMCLI = 11;",
+        "SELECT p.libPdt, COUNT(c.NUMSORT) AS nombre_commandes FROM produit p LEFT JOIN concerner c ON p.NUMPDT = c.NUMPDT GROUP BY p.NUMPDT, p.libPdt ORDER BY nombre_commandes DESC;",
+        "SELECT p.libPdt,ROUND(((MAX(CASE WHEN prix.numAnnee = 2025 THEN prix.prixVente END) - MAX(CASE WHEN prix.numAnnee = 2023 THEN prix.prixVente END)) / MAX(CASE WHEN prix.numAnnee = 2023 THEN prix.prixVente END) * 100), 2) AS evolution_pourcentage FROM produit p JOIN prix ON p.NUMPDT = prix.NUMPDT WHERE prix.numAnnee IN (2023, 2025) GROUP BY p.NUMPDT, p.libPdt ORDER BY evolution_pourcentage DESC;"
+    ]
+    
+    variableRequete = StringVar(fenetre)
+    variableRequete.set(nomsSQL[0] if nomsSQL else "")
+    
+    listeRequetes = ttk.Combobox(cadre_gauche, textvariable=variableRequete, values=nomsSQL, state="readonly", width=50)
+    listeRequetes.pack(pady=5)
+    
+    # Zone de texte pour écrire des requêtes SQL personnalisées
+    textPerso = Label(cadre_gauche, text="Ou écrire une requête SQL personnalisée")
+    textPerso.pack(pady=5)
+    
+    zoneTextPerso = Text(cadre_gauche, height=10, width=50)
+    zoneTextPerso.pack(pady=5)
+
+    boutonRequete = ttk.Button(cadre_gauche, text="Exécuter", command=lambda: executer_requete(variableRequete, zoneTextPerso, afficher_resultats, lambda text: trouverSQL(text, nomsSQL, requeteSQL),fenetre))
+    boutonRequete.pack(pady=5)
+    
+    # Section pour visualiser les tables
+
+    tables = ["annee", "client", "concerner", "entree", "prix", "produit", "saunier", "sortie"]
+
+    textTables = Label(cadre_droit, text="Visualiser une table", font=("Arial", 11, "bold"))
+    textTables.pack(pady=5)
+
+    tables = ["annee", "client", "concerner", "entree", "prix", "produit", "saunier", "sortie"]
+
+    for nom_table in tables:
+        bouton = ttk.Button(cadre_droit, text=nom_table.capitalize(), padding=(0,8), width=25,
+                        command=lambda table=nom_table: (
+                            zoneTextPerso.delete("1.0", END),
+                            zoneTextPerso.insert(END, f"SELECT * FROM {table};"),
+                            executer_requete(variableRequete, zoneTextPerso, afficher_resultats, lambda text: "NONE", fenetre),
+                            zoneTextPerso.delete("1.0", END)
+                        ))
+        bouton.pack(pady=3)
+        
+    fenetre.mainloop()
